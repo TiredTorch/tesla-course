6cd2221eb49ab89e31fb24e51a114bb5654b1657
Viktor Ivanov <viktor.ivanov@extrawest.com>
2022-06-16 12:28:24 +0300

    Added rotating cube using vanila three js

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three"
+
 
 export const App = () => {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const renderer = new WebGLRenderer();
+  renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+  );
+  
+  document.body.innerHTML = '';
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new BoxGeometry();
+  const material = new MeshBasicMaterial({
+    color: "blue"
+  });
+
+  camera.position.z = 5;
+
+  const cube = new Mesh(geometry, material);
+
+  scene.add(cube);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
+  window.addEventListener('resize',
+  () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  })
+
   return (
-    <div>App</div>
+    null
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@mui/material';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
@@ -7,6 +8,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <App />
+    <CssBaseline>
+      <App />
+    </CssBaseline>
   </StrictMode>
 );
